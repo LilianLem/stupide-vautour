1b5bbc4d953c61b69088d12083b2340017764ded
LilianLem <54894780+LilianLem@users.noreply.github.com>
2019-11-25 15:08:18 +0100

    Initialisation du projet
    Installation de webpack via un utilitaire, avec les composants Babel, Autoprefixer,  Jest, SASS...

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
